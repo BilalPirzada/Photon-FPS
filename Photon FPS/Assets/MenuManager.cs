6d8972800adbbc488a027febeb98c0d8906fb82b
Muhammad Bilal <bilal.se74@gmail.com>
2021-04-15 11:01:42 +0500

    work until 16:00

---
+++ Photon FPS/Assets/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField]
+    Menu[] menus;
+
+    void Awake()
+
+    {
+        Instance = this;
+    }
+    
+    public void OpenMenu(string _menuName)
+    {
+        for(int i=0; i<menus.Length; i++)
+        {
+            if(menus[i] == _menuName)
+            {
+                OpenMenu(menus[i]);
+            }
+            else if(menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+
+    public void OpenMenu(Menu _menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+           
+            if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+
+        _menu.Open();
+    }
+
+    public void CloseMenu(Menu _menu)
+    {
+        _menu.Close();
+    }
+
+}
